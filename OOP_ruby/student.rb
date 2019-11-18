940f9d25266b593b4722ca99c2b2ea1bdc98e988
falguniislam <islamfalguni12@gmail.com>
2019-11-18 17:49:33 +0100

    working on student file

---
+++ OOP_ruby/student.rb
@@ -0,0 +1,16 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username
+
+  @first_name
+  @last_name
+  @email
+  @username
+  @password
+
+# def first_name=(name)
+#   @first_name = name
+# end
+#
+# def first_name
+#   @first_name
+# end
