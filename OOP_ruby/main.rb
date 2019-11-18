11be4aca8f9fd088b8697237dae2ba645633d122
falguniislam <islamfalguni12@gmail.com>
2019-11-18 17:39:54 +0100

    oop ruby working

---
+++ OOP_ruby/main.rb
@@ -0,0 +1,14 @@
+# Introduction
+class User
+  attr_accessor :name, :email
+  def initialize(name, email)
+    @name = name
+    @email = email
+  end
+  def run
+    puts "Hey I'm running"
+  end
+  def self.identify_yourself
+    puts "Hey I am a class method"
+  end
+end
